f0b446a690e108fab99f51af9b8517e66b6b1b2b
Koosha <koosha@flipperz.co>
2021-05-21 18:00:45 -0700

    Applies STFT on the input.

--- VSBPowerClassification/vsbPowerClassifier.py
+++ VSBPowerClassification/vsbPowerClassifier.py
@@ -1,3 +1,32 @@
 # Source: https://datamadness.github.io/time-signal-CNN
+import matplotlib.pyplot as plt
+
+# Execute STFT on phase signal data and reduce the resulting 2D matrix
+
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import os
+import pyarrow.parquet as pq
+from scipy import signal
+import matplotlib.pyplot as plt
+from skimage.measure import block_reduce
+import math
 
 
+def signal_stft(phase_data,plot = False):
+    fs = 40e6
+    f, t, Zxx = signal.stft(phase_data, fs, nperseg=1999, boundry=None)
+
+    reducedZ = block_reduce(np.abs(Zxx), block_size=(1, 1), func=np.max)
+
+    reducedf = f[0::1]
+    reducedt = t[0::1]
+
+    if plot:
+        plt.pcolormesh(reducedt, reducedf, reducedZ, vmin=0, vmax=0.5)
+        plt.title('STFT Magnitude Reduced')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [ms]')
+        plt.show()
+    return reducedZ
